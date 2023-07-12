38538c1b008b65acfefbe62eda8ffe209cf12a35
ergoog <86678343+ernest0507@users.noreply.github.com>
2023-07-12 21:49:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const os = require('os');
 
 app.use(express.json());
 app.use(cors());
@@ -11,4 +12,5 @@ app.use('/auth', userRoot);
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => console.log("server started on port 3001"));
+    console.log(os.hostname());
 });
