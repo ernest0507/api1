ae2c45863e7f56645a6886fc688aad4dd3ed9f56
ergoog <86678343+ernest0507@users.noreply.github.com>
2023-07-12 22:45:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const db = require('./models');
 const userRoot = require('./routes/Users');
 app.use('/auth', userRoot);
 
+app.get('/', function(req, res) {
+    var domain = req.headers.host;
+    var parts  = domain.split(".");
+    var subdomain = parts.length == 2 ? "www" : parts[0]; 
+    console.log(subdomain)
+});
+
 db.sequelize.sync().then(() => {
     app.listen(3001, () => console.log("server started on port 3001"));
     console.log(os.hostname());
